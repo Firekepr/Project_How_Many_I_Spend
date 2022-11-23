8934b45aa99e0f8c0f98470feac11b166663d6f7
Firekepr <lipemlkmec@gmail.com>
2022-11-23 00:18:52 -0300

    1 - create database

---
+++ lib/database/database-tables.dart
@@ -0,0 +1,30 @@
+import 'dart:core';
+
+class DataBaseTables {
+
+  static const String calendarEvents = ''' 
+  CREATE TABLE mov_calendar_itens(
+    id integer PRIMARY KEY AUTOINCREMENT,
+    date text,
+    type text,
+    parcel integer DEFAULT 0,
+    description text
+  ) ''';
+
+  static const String currentUser = ''' 
+  CREATE TABLE mov_user(
+    id integer PRIMARY KEY AUTOINCREMENT,
+    salary text DEFAULT '',
+    dark_theme integer DEFAULT 0,
+    show_notification integer DEFAULT 0,
+    auto_complete_parcel integer DEFAULT 0,
+    user_photo text DEFAULT ''
+  )''';
+
+  static const String systemConfiguration = '''
+  CREATE TABLE mov_system(
+  id integer PRIMARY KEY AUTOINCREMENT,
+  first_time integer DEFAULT 0
+  )''';
+
+}
---
+++ lib/database/database.dart
@@ -0,0 +1,69 @@
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+import 'database-tables.dart';
+
+class DatabaseHelper {
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  static Database? _dataBase;
+  Future<Database> get database async => _dataBase ??= await _initDatabase(false);
+
+  Future<Database> _initDatabase(bool restart) async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, 'how_many_spend.db');
+
+    Database db = await openDatabase(
+      path,
+      version: 11,
+      onCreate: _onCreate,
+      readOnly: false,
+    );
+
+    return db;
+  }
+
+  Future _onCreate(Database db, int versions) async {
+    await _createTables(db);
+  }
+
+  Future<void> _createTables(Database db) async {
+    List<String> tables = [
+      DataBaseTables.calendarEvents,
+      DataBaseTables.currentUser,
+      DataBaseTables.systemConfiguration,
+    ];
+
+    try {
+      for (var table in tables) {
+        db.execute(table);
+      }
+    } catch (e) {
+      log('Erro ao criar tabelas: ${e.toString()}');
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> getSQLSelect(String sql, [List<dynamic>? parameters]) async {
+    Database db = await database;
+    return await db.rawQuery(sql, parameters);
+  }
+
+  Future<int> insert(String table, Map<String, Object?> values) async {
+    return _dataBase!.insert(table, values);
+  }
+
+  Future<int> update(
+      String table, Map<String, Object?> values, String where, var id) async {
+    return _dataBase!.update(table, values, where: where, whereArgs: [id]);
+  }
+
+  Future<int> delete(String sql) async {
+    Database db = await database;
+    return await db.rawDelete(sql);
+  }
+}
