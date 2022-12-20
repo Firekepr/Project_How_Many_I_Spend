60d7417b6029ae41526e89f7ae55bd7ed3360138
Firekepr <lipemlkmec@gmail.com>
2022-12-20 18:33:21 -0300

    Remove unecessary calendar methods

--- lib/pages/calendar/calendar.dart
+++ lib/pages/calendar/calendar.dart
@@ -1,9 +1,12 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:how_many_i_spend/pages/calendar/components/table-calendar-component.dart';
 import 'package:how_many_i_spend/services/calendar-service.dart';
-import 'package:how_many_i_spend/services/provider-calendar.dart';
+import 'package:how_many_i_spend/provider/provider-calendar.dart';
 import 'package:provider/provider.dart';
 
+import '../../database/database.dart';
 import '../../global.dart';
 
 class Calendar extends StatefulWidget {
@@ -73,16 +76,5 @@ class _CalendarState extends State<Calendar> {
 
   void _onLongDayPressed(DateTime dayOne, DateTime dayTwo) {
     _cS.addNewEventModal(context, dayOne);
-  }
-
-  Future<void> _selectDate() async {
-    DateTime? picked = await showDatePicker(
-        context: context,
-        initialDate: DateTime.now(),
-        firstDate: DateTime(2019),
-        lastDate: DateTime(2020),
-    );
-
-    print(picked);
   }
 }
